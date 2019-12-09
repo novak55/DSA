c1a1315ef656044d8e5c0fc961eaa2c7579bec52
Milan Novák <milan.novak@vspj.cz>
2019-12-09 11:06:57 +0100

    Sklad zboží init

---
+++ Sklad zboží/Sklad zboží.cpp
@@ -0,0 +1,33 @@
+﻿// Sklad zboží.cpp
+// Napište program pro vedení a sledování skladu zboží.
+// Sklad bude reprezentován jako tabulka, jejíž prvky budou obsahovat:
+//	identifikační číslo zboží, název zboží, cenu zboží a počet kusů na skladě.
+// Klíčem je identifikační číslo zboží.
+// Zvolte vhodnou reprezentaci tabulky za předpokladu, že budou často do seznamu zařazovány nové skladové zásoby
+// a odebírány již neplatné zásoby, počet zásob není předem znám.
+// Operace:
+//  -výpis prvku s daným klíčem 
+//  - zrušení prvků s daným klíčem 
+//  - výpis všech prvků seřazený podle identifikačního čísla 
+//  - výpočet celkové ceny zboží
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Sklad zboží";
+}
+
+// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
+// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
+
+// Tipy pro zahájení práce:
+//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
+//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
+//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
+//   4. K zobrazení chyb použijte okno Seznam chyb.
+//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
+//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
